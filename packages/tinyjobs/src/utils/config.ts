71472652bc37d1b6cea8c803c94c0f66242da11e
Aditya Tripathi <aditya@climactic.co>
2024-08-28 22:26:41 +0000

    chore: configs for example + valibot on tinyjobs

--- packages/tinyjobs/src/utils/config.ts
+++ packages/tinyjobs/src/utils/config.ts
@@ -1,19 +1,22 @@
 import fs from "fs";
 import path from "path";
-
-interface TinyJobConfig {
-  jobsDir: string;
-  language: "typescript" | "javascript";
-}
+import * as v from "valibot";
 
 const configFileName = "tinyjobs.json";
+
+const schema = v.object({
+  jobsDir: v.string(),
+  language: v.pipe(v.string(), v.regex(/^(typescript|javascript)$/)),
+});
 
 export const getConfig = async () => {
   if (!fs.existsSync(path.resolve(process.cwd(), configFileName))) {
     return undefined;
   }
 
-  return JSON.parse(
-    fs.readFileSync(path.resolve(process.cwd(), configFileName), "utf-8")
-  ) as TinyJobConfig;
+  const parsed = JSON.parse(
+    fs.readFileSync(path.resolve(process.cwd(), configFileName), "utf-8") ?? {}
+  );
+
+  return v.parse(schema, parsed);
 };
